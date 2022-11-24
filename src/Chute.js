60b37cc421ecaff7696e59715368a1fc2b61a14b
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-11-24 15:35:10 -0300

    implementado pre-jogo

--- src/Chute.js
+++ src/Chute.js
@@ -1,11 +1,20 @@
+import React from 'react'
+
 export default function Chute(){
+
+    const [inputValue, setInput] = React.useState("");
+
+    function chutar(){
+        alert(inputValue);
+    }
+
     return(
         <div className="chute">
             <span className="texto">
                 Já sei a palavra!
             </span>
-            <input/>
-            <button className="letras hover-anim chutar">Chutar</button>
+            <input className='input' value={inputValue} onChange={(e)=>setInput(e.target.value)}/>
+            <button className="letras hover-anim chutar" onClick={chutar}>Chutar</button>
         </div>
     )
 }
