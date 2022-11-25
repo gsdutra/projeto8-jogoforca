0f41873f6c1c17b7ce936b1fd3e1e3b7106e2545
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-11-25 11:03:29 -0300

    Ponto errado

--- src/Jogo.js
+++ src/Jogo.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import palavras from './palavras';
 
 export default function Jogo(){
 
 	const [forcaImg, setForcaImg] = React.useState(0);
+	const [palavra, setPalavra] = React.useState("");
+	const [palavraEncripted, setEncripted] = React.useState([]);
+
+	function escolherPalavra(){
+		const palavraSecreta = palavras[Math.floor(Math.random()*palavras.length)];
+		setPalavra(palavraSecreta);
+		const arrayCrypto = Array(palavraSecreta.length);
+		arrayCrypto.fill('_');
+		setEncripted(arrayCrypto);
+		console.log(palavra);
+	}
 
 	return(
 		<>
 			<div className='jogo'>
 				<img src = {`./assets/forca${forcaImg}.png`}/>
 				<div className='lado-esquerdo'>
-					<button className='escolher hover-anim'>Escolher Palavra</button>
-					<div className='palavra'>_ _ _ _ _ _ _ _ _</div>
+					<button className='escolher hover-anim' onClick={escolherPalavra}>Escolher Palavra</button>
+					<div className='palavra'>{palavraEncripted}</div>
 				</div>
 			</div>
 		</>
